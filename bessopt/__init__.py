26ff8fd48b8648565ce02d13bead67b7c1c470b8
clarkmaio <maioliandrea0@gmail.com>
2026-04-29 09:25:14 +0200

    clean init

--- bessopt/__init__.py
+++ bessopt/__init__.py
@@ -3,7 +3,5 @@
 
 
 from .battery import Battery, BatteryConstraints
-from .problems.optimisation import BESSOptimisation
 from .problems.dayahead import DAOptimisation
-from .problems.intraday import IntradayOptimisation
 from .utility import Utility
